1408e10fdceed8e3e751e2499ae1876265e28c03
Raju Ghorai <amirajubolchi001@gmail.com>
2023-01-24 19:24:19 +0530

    update config code add tags

--- src/config.go
+++ src/config.go
@@ -14,6 +14,27 @@ type Config struct {
 
 var BASE_CONFIG Config
 
+// Purity tags
+var PTAGS = map[string]string{
+	"sfw":     "100",
+	"sketchy": "010",
+	"nsfw":    "001",
+	"ws":      "110",
+	"wn":      "101",
+	"sn":      "011",
+	"all":     "111",
+}
+
+// Categories tags
+var CTAGS = map[string]string{
+	"all":     "111",
+	"anime":   "010",
+	"general": "100",
+	"people":  "001",
+	"ga":      "110",
+	"gp":      "101",
+}
+
 func init() {
 	err := godotenv.Load()
 	if err != nil {
@@ -23,7 +44,7 @@ func init() {
 	BASE_CONFIG.APIKEY = os.Getenv("API_KEY")
 }
 
-func (c Config) getBaseUrl() string {
+func (c Config) getAPIKey() string {
 	var url string
 	if c.APIKEY != "" {
 		url = c.APIURL + "?apikey=" + c.APIKEY
@@ -34,6 +55,6 @@ func (c Config) getBaseUrl() string {
 }
 
 func (c Config) GET_URL(query string) string {
-	return c.getBaseUrl() + "&" + query
+	return c.getAPIKey() + "&" + query
 
 }
